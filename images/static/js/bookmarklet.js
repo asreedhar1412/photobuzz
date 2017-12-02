23269e13a9e157ea1487b1e07864d79437f7d3d8
mdondeti <vdondeti@unomaha.edu>
2017-12-02 16:16:18 -0600

    Final Deploy.

--- images/static/js/bookmarklet.js
+++ images/static/js/bookmarklet.js
@@ -1,6 +1,6 @@
 (function(){
   var jquery_version = '2.1.4';
-  var site_url = 'http://photobuzz.com:8000/';
+  var site_url = 'https://photobuzzz.herokuapp.com/';
   var static_url = site_url + 'static/';
   var min_width = 100;
   var min_height = 100;
